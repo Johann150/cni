5101c4587a55152b9e7d146803d08092c6fb858c
Johann150 <johann.galle@protonmail.com>
2021-02-12 08:32:49 +0100

    implement raw values, fix keys
    Now allows for - and _ in keys, as in test core::key03.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This will be a parser for the [CNI configuration format](https://github.com/libuconf/cni/).
 
 ## Reference Compliance
-`core`: 26/32
-`ini`: non-compliant
-`ext`: none
+`core`: 32/32
+`ini`: fully compliant
+`ext`: tabulation
--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,123 @@
 use std::collections::HashMap;
-use std::str::Chars;
 use std::iter::Peekable;
+use std::str::Chars;
 
 #[cfg(test)]
 mod tests;
 
 fn is_vertical_ws(c: char) -> bool {
-	// Perl's / Raku's "\v"
-	matches!(c, '\n' | '\u{B}' | '\u{C}' | '\r' | '\u{85}' | '\u{2028}' | '\u{2029}')
+    // Perl's / Raku's "\v"
+    matches!(
+        c,
+        '\n' | '\u{B}' | '\u{C}' | '\r' | '\u{85}' | '\u{2028}' | '\u{2029}'
+    )
 }
 
 fn skip_ws(chars: &mut Peekable<Chars>) {
-	while matches!(chars.peek(), Some(c) if c.is_whitespace()) {
-		chars.next();
-	}
+    while matches!(chars.peek(), Some(c) if c.is_whitespace()) {
+        chars.next();
+    }
 }
 
 fn parse_key(chars: &mut Peekable<Chars>) -> Result<String, &'static str> {
-	// because chars is a &mut, the iterator is not moved and can still be used
-	// after this call
-	let mut key = String::new();
+    // because chars is a &mut, the iterator is not moved and can still be used
+    // after this call
+    let mut key = String::new();
 
-	while matches!(chars.peek(), Some('0'..='9')|Some('a'..='z')|Some('A'..='Z')|Some('.')) {
-		key.push(chars.next().unwrap());
-	}
+    while matches!(
+        chars.peek(),
+        Some('0'..='9') | Some('a'..='z') | Some('A'..='Z') | Some('-') | Some('_') | Some('.')
+    ) {
+        key.push(chars.next().unwrap());
+    }
 
-	if key.starts_with('.') || key.ends_with('.') {
-		// key cannot start or end with a dot
-		Err("invalid key")
-	} else if key.is_empty() {
-		Err("expected key")
-	} else {
-		Ok(key)
-	}
+    if key.starts_with('.') || key.ends_with('.') {
+        // key cannot start or end with a dot
+        Err("invalid key")
+    } else if key.is_empty() {
+        Err("expected key")
+    } else {
+        Ok(key)
+    }
 }
 
 fn parse_value(chars: &mut Peekable<Chars>) -> Result<String, &'static str> {
-	let mut value = String::new();
+    let mut value = String::new();
 
-	while matches!(chars.peek(), Some(c) if c.is_alphanumeric()||(c.is_whitespace()&&!is_vertical_ws(*c))) {
-		value.push(chars.next().unwrap());
-	}
+    if chars.peek() == Some(&'`') {
+        // raw value
+        chars.next(); // consume backtick
+        loop {
+            if chars.peek() == Some(&'`') {
+                // check if this is an escaped backtick
+                chars.next();
+                if chars.peek() == Some(&'`') {
+                    // escaped backtick
+                    chars.next();
+                    value.push('`');
+                } else {
+                    // end of the value
+                    break;
+                }
+            } else if let Some(c) = chars.next() {
+                value.push(c);
+            } else {
+                // current value must have been a None
+                return Err("unterminated raw value");
+            }
+        }
+    } else {
+        // normal value: alphanumerics or horizontal whitespace
+        while matches!(chars.peek(), Some(c) if c.is_alphanumeric()||(c.is_whitespace()&&!is_vertical_ws(*c)))
+        {
+            value.push(chars.next().unwrap());
+        }
+    }
 
-	Ok(value.trim().to_string())
+    Ok(value.trim().to_string())
 }
 
 pub fn parse(text: &str) -> Result<HashMap<String, String>, &'static str> {
-	let mut chars = text.chars().peekable();
-	let mut map = HashMap::new();
-	let mut section = String::new();
+    let mut chars = text.chars().peekable();
+    let mut map = HashMap::new();
+    let mut section = String::new();
 
-	while let Some(&c) = chars.peek() {
-		if c.is_whitespace() /* same as Perl's and Raku's \s */ {
-			chars.next(); // consume whitespace
-		} else if c == '#' || c == ';' {
-			// comment, continue until next vertical whitespace or EOF
-			while matches!(chars.next(), Some(c) if !is_vertical_ws(c)) {}
-		} else if c == '[' {
-			chars.next(); // consume [
-			skip_ws(&mut chars);
-			section = parse_key(&mut chars)?;
-			skip_ws(&mut chars);
-			if chars.next() != Some(']') {
-				return Err("expected \"]\"")
-			}
-		} else {
-			// this should be a key
-			let mut key = parse_key(&mut chars)?;
-			if !section.is_empty() {
-				// prepend section name
-				key = format!("{}.{}", section, key);
-			}
+    while let Some(&c) = chars.peek() {
+        if c.is_whitespace()
+        /* same as Perl's and Raku's \s */
+        {
+            chars.next(); // consume whitespace
+        } else if c == '#' || c == ';' {
+            // comment, continue until next vertical whitespace or EOF
+            while matches!(chars.next(), Some(c) if !is_vertical_ws(c)) {}
+        } else if c == '[' {
+            // section heading
+            chars.next(); // consume [
+            skip_ws(&mut chars);
+            section = parse_key(&mut chars)?;
+            skip_ws(&mut chars);
+            if chars.next() != Some(']') {
+                return Err("expected \"]\"");
+            }
+        } else {
+            // this should be a key
+            let mut key = parse_key(&mut chars)?;
+            if !section.is_empty() {
+                // prepend section name
+                key = format!("{}.{}", section, key);
+            }
 
-			skip_ws(&mut chars);
-			if chars.next() != Some('=') {
-				return Err("expected \"=\"");
-			}
-			skip_ws(&mut chars);
+            skip_ws(&mut chars);
+            if chars.next() != Some('=') {
+                return Err("expected \"=\"");
+            }
+            skip_ws(&mut chars);
 
-			let value = parse_value(&mut chars)?;
+            let value = parse_value(&mut chars)?;
 
-			map.insert(key, value);
-		}
-	}
+            map.insert(key, value);
+        }
+    }
 
-	Ok(map)
+    Ok(map)
 }
