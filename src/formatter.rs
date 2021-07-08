ec6a681c18f1947daab7c5361c082cab88877664
Johann150 <johann.galle@protonmail.com>
2021-07-08 19:32:40 +0200

    fix formatter grouping
    - never use the built in serializer as that might produce subsection headings
    - use the correct api to get the subsection grouping correctly

--- src/formatter.rs
+++ src/formatter.rs
@@ -3,6 +3,20 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
+pub fn print_values(map: &HashMap<String, String>) {
+    // don't use cni_format::to_str so there are no section headings
+    for (key, value) in map {
+        print!("{} = ", key);
+        if value.is_empty() {
+            println!("#empty");
+        } else if value.contains(|c| c=='`'||c=='#'||c==';'||crate::linter::is_vertical_ws(&c)) {
+            println!("`{}`", value.replace("`", "``"));
+        } else {
+            println!("{}", value);
+        }
+    }
+}
+
 pub fn format(file: &str, section_threshold: usize, opts: cni_format::Opts) {
     let stream: Box<dyn Read> = if file == "-" {
         Box::new(std::io::stdin())
@@ -26,34 +40,24 @@ pub fn format(file: &str, section_threshold: usize, opts: cni_format::Opts) {
             }
         };
 
-    // print the leaves in the top level
-    print!("{}", cni_format::to_str(map.sub_leaves("")));
-    map.retain(|key, _| key.contains('.'));
+    if section_threshold > 0 {
+        // print the leaves in the top level
+        print_values(&map.sub_leaves(""));
+        map.retain(|key, _| key.contains('.'));
 
-    let mut sections = map.sub_tree("").keys().cloned().collect::<Vec<_>>();
-    sections.sort_unstable_by(|a, b|
-        // long before short, then alphabetically
-        a.len().cmp(&b.len()).reverse().then_with(|| a.cmp(b)));
-    for section in sections {
-        if map.sub_tree(&section).len() >= section_threshold {
-            print!(
-                "[{}]\n{}",
-                section,
-                cni_format::to_str(map.sub_tree(&section))
-            );
-            map.retain(|key, _| !key.starts_with(&format!("{}.", section)));
+        let mut sections = map.section_tree("").iter().cloned().collect::<Vec<_>>();
+        sections.sort_unstable_by(|a, b|
+            // long before short, then alphabetically
+            a.len().cmp(&b.len()).reverse().then_with(|| a.cmp(b)));
+        for section in sections {
+            if map.sub_tree(&section).len() >= section_threshold {
+                println!("[{}]", section);
+                print_values(&map.sub_tree(&section));
+                map.retain(|key, _| !key.starts_with(&format!("{}.", section)));
+            }
         }
     }
 
-    // don't use cni_format::to_str here so there are no section headings
-    for (key, value) in map {
-        print!("{} = ", key);
-        if value.is_empty() {
-            println!("#empty");
-        } else if value.contains(|c| c=='`'||c=='#'||c==';'||crate::linter::is_vertical_ws(&c)) {
-            println!("`{}`", value.replace("`", "``"));
-        } else {
-            println!("{}", value);
-        }
-    }
+    // print the remaining values
+    print_values(&map);
 }
