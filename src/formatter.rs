0ffc3647b47ce640d855f3b980c2d9846a38f044
Johann150 <johann.galle@protonmail.com>
2021-07-08 21:05:18 +0200

    merge format and dump subcommands

--- src/formatter.rs
+++ src/formatter.rs
@@ -3,13 +3,24 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
-pub fn print_values(map: &HashMap<String, String>) {
+pub enum Format {
+    /// The section threshold, i.e. how many items before a section heading
+    /// is used. Zero means no section headings should be used.
+    Cni(usize),
+    /// If the first is None, the key is not printed.
+    /// If the second is None, the value is not printed.
+    Custom(Option<String>, Option<String>, String),
+}
+
+fn print_cni(map: &HashMap<String, String>) {
     // don't use cni_format::to_str so there are no section headings
     for (key, value) in map {
         print!("{} = ", key);
         if value.is_empty() {
             println!("#empty");
-        } else if value.contains(|c| c=='`'||c=='#'||c==';'||crate::linter::is_vertical_ws(&c)) {
+        } else if value
+            .contains(|c| c == '`' || c == '#' || c == ';' || crate::linter::is_vertical_ws(&c))
+        {
             println!("`{}`", value.replace("`", "``"));
         } else {
             println!("{}", value);
@@ -17,47 +28,65 @@ pub fn print_values(map: &HashMap<String, String>) {
     }
 }
 
-pub fn format(file: &str, section_threshold: usize, opts: cni_format::Opts) {
-    let stream: Box<dyn Read> = if file == "-" {
-        Box::new(std::io::stdin())
-    } else {
-        match File::open(&file) {
-            Ok(f) => Box::new(f),
-            Err(e) => {
-                eprintln!("{:?}: {}", file, e);
-                std::process::exit(1);
+pub fn format(files: clap::Values, format: Format, opts: cni_format::Opts) {
+    let map = files
+        .flat_map(|file| {
+            let stream: Box<dyn Read> = if file == "-" {
+                Box::new(std::io::stdin())
+            } else {
+                match File::open(&file) {
+                    Ok(f) => Box::new(f),
+                    Err(e) => {
+                        eprintln!("{:?}: {}", file, e);
+                        std::process::exit(1);
+                    }
+                }
+            };
+
+            let stream =
+                utf::decode_utf8(stream.bytes().filter_map(Result::ok)).filter_map(Result::ok);
+            cni_format::CniParser::new_opts(stream, opts)
+        })
+        .collect::<Result<HashMap<_, _>, _>>();
+
+    if let Err(e) = map {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+    let mut map = map.unwrap();
+
+    match format {
+        Format::Cni(0) => print_cni(&map),
+        Format::Cni(section_threshold) => {
+            // print the leaves in the top level
+            print_cni(&map.sub_leaves(""));
+            map.retain(|key, _| key.contains('.'));
+
+            let mut sections = map.section_tree("").iter().cloned().collect::<Vec<_>>();
+            sections.sort_unstable_by(|a, b|
+                // long before short, then alphabetically
+                a.len().cmp(&b.len()).reverse().then_with(|| a.cmp(b)));
+            for section in sections {
+                if map.sub_tree(&section).len() >= section_threshold {
+                    println!("[{}]", section);
+                    print_cni(&map.sub_tree(&section));
+                    map.retain(|key, _| !key.starts_with(&format!("{}.", section)));
+                }
             }
+
+            // print the remaining values
+            print_cni(&map);
         }
-    };
-
-    let stream = utf::decode_utf8(stream.bytes().filter_map(Result::ok)).filter_map(Result::ok);
-    let mut map: HashMap<String, String> =
-        match cni_format::CniParser::new_opts(stream, opts).collect() {
-            Ok(map) => map,
-            Err(e) => {
-                eprintln!("{}", e);
-                std::process::exit(1);
-            }
-        };
-
-    if section_threshold > 0 {
-        // print the leaves in the top level
-        print_values(&map.sub_leaves(""));
-        map.retain(|key, _| key.contains('.'));
-
-        let mut sections = map.section_tree("").iter().cloned().collect::<Vec<_>>();
-        sections.sort_unstable_by(|a, b|
-            // long before short, then alphabetically
-            a.len().cmp(&b.len()).reverse().then_with(|| a.cmp(b)));
-        for section in sections {
-            if map.sub_tree(&section).len() >= section_threshold {
-                println!("[{}]", section);
-                print_values(&map.sub_tree(&section));
-                map.retain(|key, _| !key.starts_with(&format!("{}.", section)));
+        Format::Custom(pre, mid, post) => {
+            for (key, value) in map {
+                if let Some(ref pre) = pre {
+                    print!("{}{}", key, pre);
+                }
+                if let Some(ref mid) = mid {
+                    print!("{}{}", value, mid);
+                }
+                print!("{}", post);
             }
         }
     }
-
-    // print the remaining values
-    print_values(&map);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::{crate_authors, crate_description, crate_version, App, AppSettings, Arg, SubCommand};
 use std::collections::HashMap;
 
-mod dumper;
 mod formatter;
 mod iter;
 mod linter;
@@ -49,80 +48,50 @@ fn main() {
                 )
         )
         .subcommand(
-            SubCommand::with_name("dump")
+        SubCommand::with_name("format")
                 .setting(AppSettings::UnifiedHelpMessage)
-                .about("reads in CNI files and shows the combined result in the specified format")
+                .visible_alias("fmt")
+                .about("Reads in CNI files and shows the combined result in the specified format.")
                 .arg(
                     Arg::with_name("cni")
-                        .help("The output format should be CNI. [default]")
-                        .overrides_with_all(&["csv", "null", "prefix", "infix", "postfix", "postfix-nonl"])
+                        .help("The output format should be CNI. Equivalent to --format=\"KEY = `VALUE`\". [default]")
+                        .overrides_with_all(&["csv", "null", "format"])
                         .long("cni")
                 )
                 .arg(
+                    Arg::with_name("threshold")
+                        .help("Specifies the threshold of how many entries have to be in a section to make use of a section header. 0 means no section headers will be used.")
+                        .long("section-threshold")
+                        .short("n")
+                        .default_value("10")
+                        .validator(|arg| arg.parse::<usize>().map(|_| ()).map_err(|e| e.to_string()))
+                        .requires("cni")
+                )
+                .arg(
                     Arg::with_name("csv")
-                        .help("The output format should be comma separated values.")
-                        .overrides_with_all(&["cni", "null", "prefix", "infix", "postfix", "postfix-nonl"])
+                        .help("The output format should be comma separated values. Equivalent to --format=\"KEY,VALUE\"")
+                        .overrides_with_all(&["cni", "null", "format"])
                         .long("csv")
                         .short("c")
                 )
                 .arg(
                     Arg::with_name("null")
                         .help("Records are terminated by a null character instead of a line feed to better accomodate values containing line feeds.")
-                        .overrides_with_all(&["cni", "csv", "prefix", "infix", "postfix", "postfix-nonl"])
+                        .overrides_with_all(&["cni", "csv", "format"])
                         .long("null")
                         .short("0")
                 )
                 .arg(
-                    Arg::with_name("prefix")
-                        .help("Specifies a custom line prefix. Can be used together with --infix and --postfix.")
+                    Arg::with_name("format")
+                        .help("Sets a custom format. KEY and VALUE are placeholders and may not occur more than once.")
                         .overrides_with_all(&["cni", "csv", "null"])
-                        .long("prefix")
-                        .value_name("prefix")
-                )
-                .arg(
-                    Arg::with_name("infix")
-                        .help("Specifies a custom line prefix. Can be used together with --prefix and --postfix.")
-                        .overrides_with_all(&["cni", "csv", "null"])
-                        .long("infix")
-                        .value_name("infix")
-                )
-                .arg(
-                    Arg::with_name("postfix")
-                        .help("Specifies a custom line postfix, but a line feed will be added to the specified string. Can be used together with --prefix and --infix.")
-                        .overrides_with_all(&["cni", "csv", "null", "postfix-nonl"])
-                        .long("postfix")
-                        .value_name("postfix")
-                )
-                .arg(
-                    Arg::with_name("postfix-nonl")
-                        .help("Specifies a custom line postfix, no lin feed will be added. Can be used together with --prefix and --infix.")
-                        .overrides_with_all(&["cni", "csv", "null", "postfix"])
-                        .long("postfixx")
-                        .value_name("postfix")
+                        .long("format")
+                        .takes_value(true)
                 )
                 .arg(
                     Arg::with_name("FILES")
                         .help("The input files to read. '-' will result in stdin being read.")
                         .multiple(true)
-                        .default_value("-")
-                )
-        )
-        .subcommand(
-            SubCommand::with_name("format")
-                .setting(AppSettings::UnifiedHelpMessage)
-                .alias("fmt")
-                .about("Reads in a CNI file and outputs a strictly formatted representation of the input")
-                .arg(
-                    Arg::with_name("threshold")
-                        .help("Specifies the threshold of how many entries have to be in a section to make use of a section header.")
-                        .long("section-threshold")
-                        .short("n")
-                        .default_value("10")
-                        .validator(|arg| arg.parse::<usize>().map(|_| ()).map_err(|e| e.to_string()))
-                )
-                .arg(
-                    Arg::with_name("FILE")
-                        .help("The input file to read. '-' will result in stdin being read.")
                         .default_value("-")
                 )
         )
@@ -169,37 +138,33 @@ fn main() {
                 }
             }
         }
-        ("dump", Some(matches)) => {
+        ("format", Some(matches)) => {
+            use formatter::Format;
+
             let format = if matches.is_present("csv") {
-                ("", ",\"", "\"\n")
+                Format::Custom(Some("".into()), Some(",\"".into()), "\"\n".into())
             } else if matches.is_present("null") {
-                ("", "=", "\0")
-            } else if matches.is_present("prefix")
-                || matches.is_present("infix")
-                || matches.is_present("postfix")
-                || matches.is_present("postfix-nonl")
-            {
-                (
-                    matches.value_of("prefix").unwrap_or(""),
-                    matches.value_of("infix").unwrap_or(" "),
-                    matches
-                        .value_of("postfix")
-                        .or_else(|| matches.value_of("postfix-nonl"))
-                        .unwrap_or("\n"),
+                Format::Custom(Some("".into()), Some("=".into()), "\0".into())
+            } else if matches.is_present("format") {
+                let format = format!("{}\n", matches.value_of("format").unwrap());
+                let key_pos = format.find("KEY");
+                let val_pos = format.find("VALUE");
+
+                Format::Custom(
+                    key_pos.map(|i| format[..i].into()),
+                    val_pos.map(|i| format[key_pos.unwrap_or(0)..i].into()),
+                    format[val_pos.or(key_pos).unwrap_or(0)..].into(),
                 )
             } else {
                 // must be the default CNI formatting
-                ("", " = `", "`")
+
+                // the first unwrap is okay because there is a default value in clap
+                // the second unwrap is okay because of the validator in clap
+                let section_threshold = matches.value_of("threshold").unwrap().parse().unwrap();
+                Format::Cni(section_threshold)
             };
 
-            dumper::dump(matches.values_of("FILES").unwrap(), format, opts);
-        }
-        ("format", Some(matches)) => {
-            // the first unwrap is okay because there is a default value in clap
-            // the second unwrap is okay because of the validator in clap
-            let section_threshold = matches.value_of("threshold").unwrap().parse().unwrap();
-
-            formatter::format(matches.value_of("FILE").unwrap(), section_threshold, opts);
+            formatter::format(matches.values_of("FILES").unwrap(), format, opts);
         }
         _ => unreachable!("unknown subcommand"),
     }
