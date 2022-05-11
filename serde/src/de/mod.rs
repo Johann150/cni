279c039d32995ed8d3c6674aad88c08a7b8021ce
Johann150 <johann.galle@protonmail.com>
2022-05-11 19:00:21 +0200

    remove separate deserialisation value
    All the deserialisation is now handled by the Deserializer, which
    gets rid of some duplicated code.

--- serde/src/de/mod.rs
+++ serde/src/de/mod.rs
@@ -10,13 +10,11 @@ use serde::{
 use std::collections::HashMap;
 use std::str::Chars;
 
-/// A module that deserializes a single value.
-mod value;
-
 #[derive(Debug)]
 enum Tree {
     Map(HashMap<String, Tree>),
-    Value(value::Value),
+    /// stringized value and starting position as line, column (counting from 1)
+    Value(String, usize, usize),
 }
 
 #[derive(Debug)]
@@ -31,7 +29,7 @@ impl Deserializer {
         let end = map
             .values()
             .filter_map(|v| {
-                if let Tree::Value(value::Value(_, line, col)) = v {
+                if let Tree::Value(_, line, col) = v {
                     Some((*line, *col))
                 } else {
                     None
@@ -44,23 +42,37 @@ impl Deserializer {
     }
 }
 
-macro_rules! forward {
-    ($deser:ident) => {
+macro_rules! deserialize {
+    ($deser:ident, $visit:ident, $err:ident) => {
         fn $deser<V>(self, visitor: V) -> Result<V::Value>
         where
             V: Visitor<'de>,
         {
-            if let Some(Tree::Value(value)) = self.vals.pop() {
-                value.$deser(visitor)
-            } else {
-                Err(Error {
-                    line: self.end.map_or(0, |x| x.0),
-                    col: self.end.map_or(0, |x| x.1),
-                    kind: Kind::ExpectedValues,
-                })
+            let (value, line, col) = self.next()?;
+            match value.parse() {
+                Ok(int) => visitor.$visit(int),
+                Err(err) => Err(Error {
+                    line,
+                    col,
+                    kind: Kind::$err(err),
+                }),
             }
         }
     };
+}
+
+impl Deserializer {
+    fn next(&mut self) -> Result<(String, usize, usize)> {
+        if let Some(Tree::Value(value, line, col)) = self.vals.pop() {
+            Ok((value, line, col))
+        } else {
+            Err(Error {
+                line: self.end.map_or(0, |x| x.0),
+                col: self.end.map_or(0, |x| x.1),
+                kind: Kind::ExpectedValues,
+            })
+        }
+    }
 }
 
 impl<'de, 'a> serde::Deserializer<'de> for &'a mut Deserializer {
@@ -74,7 +86,7 @@ impl<'de, 'a> serde::Deserializer<'de> for &'a mut Deserializer {
     {
         match self.vals.pop() {
             Some(Tree::Map(map)) => visitor.visit_map(&mut Deserializer::new(map)),
-            Some(Tree::Value(value)) => value.deserialize_any(visitor),
+            Some(Tree::Value(val, ..)) => visitor.visit_string(val),
             None => Err(Error {
                 line: self.end.map_or(0, |x| x.0),
                 col: self.end.map_or(0, |x| x.1),
@@ -87,27 +99,109 @@ impl<'de, 'a> serde::Deserializer<'de> for &'a mut Deserializer {
     where
         V: Visitor<'de>,
     {
-        self.vals.pop();
-        visitor.visit_unit()
+        if let Some(Tree::Value(..)) = self.vals.pop() {
+            visitor.visit_unit()
+        } else {
+            Err(Error {
+                line: self.end.map_or(0, |x| x.0),
+                col: self.end.map_or(0, |x| x.1),
+                kind: Kind::ExpectedValues,
+            })
+        }
     }
 
-    // take next value and forward deserialisation to it
-    forward!(deserialize_u8);
-    forward!(deserialize_u16);
-    forward!(deserialize_u32);
-    forward!(deserialize_u64);
-    forward!(deserialize_i8);
-    forward!(deserialize_i16);
-    forward!(deserialize_i32);
-    forward!(deserialize_i64);
-    forward!(deserialize_f32);
-    forward!(deserialize_f64);
-    forward!(deserialize_bool);
-    forward!(deserialize_char);
-    forward!(deserialize_bytes);
-    forward!(deserialize_byte_buf);
-    forward!(deserialize_option);
-    forward!(deserialize_unit);
+    deserialize!(deserialize_i8, visit_i8, Int);
+    deserialize!(deserialize_i16, visit_i16, Int);
+    deserialize!(deserialize_i32, visit_i32, Int);
+    deserialize!(deserialize_i64, visit_i64, Int);
+    deserialize!(deserialize_u8, visit_u8, Int);
+    deserialize!(deserialize_u16, visit_u16, Int);
+    deserialize!(deserialize_u32, visit_u32, Int);
+    deserialize!(deserialize_u64, visit_u64, Int);
+    deserialize!(deserialize_f32, visit_f32, Float);
+    deserialize!(deserialize_f64, visit_f64, Float);
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        let (mut val, line, col) = self.next()?;
+
+        val.make_ascii_lowercase();
+        match val.as_str() {
+            "1" | "+" | "true" | "yes" | "on" | "up" => visitor.visit_bool(true),
+            "0" | "-" | "false" | "no" | "off" | "down" => visitor.visit_bool(false),
+            _ => Err(Error {
+                line,
+                col,
+                kind: Kind::Bool,
+            }),
+        }
+    }
+
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if let Some(Tree::Value(value, line, col)) = self.vals.pop() {
+            if value.chars().count() == 1 {
+                visitor.visit_char(value.chars().next().unwrap())
+            } else {
+                Err(Error {
+                    line,
+                    col,
+                    kind: Kind::Char,
+                })
+            }
+        } else {
+            Err(Error {
+                line: self.end.map_or(0, |x| x.0),
+                col: self.end.map_or(0, |x| x.1),
+                kind: Kind::ExpectedValues,
+            })
+        }
+    }
+
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_byte_buf(self.next()?.0.into())
+    }
+
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_byte_buf(self.next()?.0.into())
+    }
+
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if self.next()?.0.is_empty() {
+            visitor.visit_none()
+        } else {
+            visitor.visit_some(self)
+        }
+    }
+
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        let (val, line, col) = self.next()?;
+        if val.is_empty() {
+            visitor.visit_unit()
+        } else {
+            Err(Error {
+                line,
+                col,
+                kind: Kind::Unit,
+            })
+        }
+    }
 
     fn deserialize_unit_struct<V>(self, _name: &'static str, visitor: V) -> Result<V::Value>
     where
@@ -201,7 +295,7 @@ where
         let result = result?;
         // can unwrap here because the parser must have returned a Ok result
         let pos = parser.last_pos().unwrap();
-        let val = value::Value(result.1, pos.0, pos.1);
+        let val = (result.1, pos.0, pos.1);
 
         // the format itself allows this, but handle duplicate keys as an error
         // because it might have unintended consequences
@@ -223,11 +317,11 @@ where
     T::deserialize(&mut Deserializer::new(obj))
 }
 
-fn to_tree(data: HashMap<String, value::Value>) -> Tree {
+fn to_tree(data: HashMap<String, (String, usize, usize)>) -> Tree {
     let mut map = data
         .sub_leaves("")
         .into_iter()
-        .map(|(key, value)| (key.to_string(), Tree::Value(value)))
+        .map(|(key, (val, line, col))| (key.to_string(), Tree::Value(val, line, col)))
         .collect::<HashMap<_, _>>();
     map.extend(data.section_leaves("").into_iter().map(|sect| {
         let tree = to_tree(data.sub_tree(&sect));
